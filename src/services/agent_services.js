ca614269db7386b109bedd10abd36792310afe21
Zaheer Jan <zaheerjan0@gmail.com>
2023-11-30 15:36:57 +0500

    axios fix

--- src/services/agent_services.js
+++ src/services/agent_services.js
@@ -3,62 +3,65 @@ import store from "../store/store";
 
 let base_url = import.meta.env.VITE_BASE_URL
 
-const getToken = () => store.getters.getToken
-// let TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiYzJjZjQzNDdkOTNkNDlkOTAwZTZiNTFmM2YzMmUwOWY0MWU5MWE1NzViZGRjOGMzMDMwYjhmY2VhZTQ1ZWMxMTVjNDNmMDg2ODViZDFlYTIiLCJpYXQiOjE3MDA3MjgzMzUuNzE5NjI3LCJuYmYiOjE3MDA3MjgzMzUuNzE5NjMyLCJleHAiOjE3MzIzNTA3MzUuNjc3Mzk5LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.iJKyeCL4pPxC4Uz0YzVjzsG2kB26vLnNTTaacREdS4IgGm1wp07P2RCVsc58N-OfI6UhfA_AEF9SPRVQOSvcQnN9oCklQcVw0AX0yjylcIwqp4x0M-wKxjUpG_jgSgoYd0mPMBGYiDsuKK0P-ozvC5HmTqtPGk7r8rLsJCzv7RCvQR4uU4P9_zcLQiprOLOyp77QZTUz4tvCqJzOICMcweVAc5tLiq5evyktQQ-84z1oAqEhDD72pWENHrAekefhtJhiDcBMC-r1QBpgvulC7r74NHEXW7cgcYNsCF698YFBTal9S8I2piiYPXkkz60j4hZ9oMkfmlNs4ROH66ILr4pLaQ7LOokr0e5lGNVE6ao_37cbvDj7NaCT4nQSsp8bi9mC7cXFG-lcl5Hbz0-aaTacGOGwt2kcp6llct4Q5rsYaIU5zNzhb_dHkVgSEq10dzgivQ6NTTbZCEariap-ODJPJfeqeKsWPc4fSsuiWf-tTtI6sIEyVH9VKECkXYK9kHO4pbsivFFdsk9Gyx-0v-wtU8YEOXrmPBpRoU2cf1ITBU9pDKKRmXDBihd9QmXOxnulvMt7uuq0EsAk57UiC4rjZp8vsgcAf_mq68s5KhS_LOlBo4LAJqKbsanKVJHAcdOndEXoKLsLcgLZb2blxxnDT7yMl_A5kcU9fBfswkc"
-let TOKEN = getToken();
-
-let HEADERS = {
-    "Authorization": `Bearer ${TOKEN}`
-}
+axios.interceptors.request.use(
+  (config) => {
+    config.headers[
+      "Authorization"
+    ] = `Bearer ${store.getters.getToken}`;
+    return Promise.resolve(config);
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export async function agentList(page=1) {
-    const resp = await axios.get(`${base_url}/api/agents?page=${page}`, {headers: HEADERS})
+    const resp = await axios.get(`${base_url}/api/agents?page=${page}`)
     return resp.data
 }
 
 export async function agentListForm(size=1000) {
-    const resp = await axios.get(`${base_url}/api/agents?size=${size}`, {headers: HEADERS})
+    const resp = await axios.get(`${base_url}/api/agents?size=${size}`)
     return resp.data
 }
 
 export async function createAgent(payload) {
-    const resp = await axios.post(`${base_url}/api/add-agent`, payload, {headers: HEADERS})
+    const resp = await axios.post(`${base_url}/api/add-agent`, payload)
     return resp.data
 }
 
 export async function agentUpdate(payload) {
-    const resp = await axios.post(`${base_url}/api/edit-agent`, payload, {headers: HEADERS})
+    const resp = await axios.post(`${base_url}/api/edit-agent`, payload)
     return resp.data
 }
 
 export async function fetchClusters(id) {
-    const resp = await axios.get(`${base_url}/api/cluster?group=${id}`, {headers: HEADERS})
+    const resp = await axios.get(`${base_url}/api/cluster?group=${id}`)
     return resp.data
 }
 
 export async function fetchSessions(group_id) {
-    const resp = await axios.get(`${base_url}/api/links?group=${group_id}`, {headers: HEADERS})
+    const resp = await axios.get(`${base_url}/api/links?group=${group_id}`)
     return resp.data
 }
 
 export async function fetchAgentlinks(id) {
-    const resp = await axios.get(`${base_url}/api/agentlinks?aid=${id}`, {headers: HEADERS})
+    const resp = await axios.get(`${base_url}/api/agentlinks?aid=${id}`)
     return resp.data
 }
 
 export async function fetchGroups() {
-    const resp = await axios.get(`${base_url}/api/groups`, {headers: HEADERS})
+    const resp = await axios.get(`${base_url}/api/groups`)
     return resp.data
 }
 export async function fetchGroupData() {
-    let resp = await axios.get(`${base_url}/api/groups`, {headers: HEADERS})
+    let resp = await axios.get(`${base_url}/api/groups`)
     const linkData = {};
     for (let i = 0; i < resp.data.length; i++) {
         const group = resp.data[i];
         try {
           const linksResp = await axios.get(
-            `${base_url}/api/links?group=${group.id}`,
-            { headers: HEADERS }
+            `${base_url}/api/links?group=${group.id}`
           );
           linkData[group.id] = linksResp.data;
         } catch (linksError) {
@@ -71,14 +74,13 @@ export async function fetchGroupData() {
 }
 
 export async function fetchClustersData() {
-    let resp = await axios.get(`${base_url}/api/groups`, {headers: HEADERS})
+    let resp = await axios.get(`${base_url}/api/groups`)
     const clusterdata = {};
     for (let i = 0; i < resp.data.length; i++) {
         const group = resp.data[i];
         try {
           const linksResp = await axios.get(
-            `${base_url}/api/cluster?group=${group.id}`,
-            { headers: HEADERS }
+            `${base_url}/api/cluster?group=${group.id}`
           );
           clusterdata[group.id] = linksResp.data;
         } catch (linksError) {
@@ -90,6 +92,6 @@ export async function fetchClustersData() {
     return clusterdata
 }
 export async function agentRefSessions(id){
-    const resp = await axios.get(`${base_url}/api/get-ref-sessions?aid=` + id, {headers: HEADERS})
+    const resp = await axios.get(`${base_url}/api/get-ref-sessions?aid=` + id)
     return resp.data
 }
