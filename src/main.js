e5dbdc7edb883870cee3b140ae04754d95773183
Atif Badini <badiniatif@gmail.com>
2026-02-06 00:28:32 +0500

    auth UI update and header Ui and some more changes

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import "bootstrap/dist/js/bootstrap.js";
 import 'font-awesome/css/font-awesome.css'
 import '@fortawesome/fontawesome-free/css/all.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 import store from '../src/store/store'
 import { createAuth0 } from '@auth0/auth0-vue'
 
